5e28ae39e91eb70b1f623092773c1059eb21661c
柳佚 <yly287738@alibaba-inc.com>
2024-06-06 12:16:08 +0800

    data organization add

--- data/data_build_stocks/organize_conversation_history.py
+++ data/data_build_stocks/organize_conversation_history.py
@@ -1,5 +1,5 @@
 from meta_icl.utils.sys_prompt_utils import load_csv, sav_json
-from meta_icl.utils.utils import get_current_date
+from meta_icl.utils.utils import get_current_date, combine_session
 import pandas as pd
 import os
 
@@ -13,33 +13,3 @@ if __name__ == '__main__':
     group_by_filed = "session_id"
 
     combine_session(csv_pth, json_sav_dir, group_by_filed, selection_filed=None)
-
-    """
-    
-    data_pth = "data/data_build_stocks/用户历史对话记录.csv"
-    data = pd.read_csv(data_pth)
-    print(data.keys())
-    sav_dir = "data/data_build_stocks"
-
-    grouped = data.groupby("session_id")
-    sessions = []
-    for session_id, group in grouped:
-        tmp = group.to_dict(orient='records')
-        conversations = []
-        print(tmp)
-        print(len(tmp))
-
-        # break
-        for item in tmp:
-            conversations.append({
-                "query": item["query"],
-                "answer": item["answer"]
-            })
-        session_dict = {
-            'session_id': session_id,
-            'conversations': conversations  # List of rows for this session
-        }
-
-        sessions.append(session_dict)
-    sav_json(sessions, os.path.join(sav_dir, "organize_conversation_history.json"))
-    """
