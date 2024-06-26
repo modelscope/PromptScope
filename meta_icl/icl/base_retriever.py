a7729364da92fc45fdfbc77f9cddf0c853698303
柳佚 <yly287738@alibaba-inc.com>
2024-06-26 18:03:17 +0800

    reorganize retriever & ICL lcass

--- meta_icl/icl/base_retriever.py
+++ meta_icl/icl/base_retriever.py
@@ -1,13 +1,32 @@
 from typing import List, Dict
+from abc import ABC, abstractmethod
 
 from meta_icl.utils.utils import sample_elements_and_ids, random_selection_method
-from meta_icl.utils.sys_prompt_utils import get_embedding, find_top_k_embeddings
+from meta_icl.utils.sys_prompt_utils import (get_embedding, find_top_k_embeddings, message_formatting,
+                                             call_llm_with_message)
+from meta_icl.utils.utils import load_file
 
 
-class BaseRetriever:
+class BaseRetriever(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def topk_selection(self):
+        pass
+
+
+class BM25Retriever(BaseRetriever):
+    def __init__(self, example_list=None):
+        self.example_list = example_list
+        super().__init__()
+
+
+class CosineSimilarityRetriever(BaseRetriever):
     def __init__(self, embeddings: List, example_list=None):
         self.embeddings = embeddings
         self.example_list = example_list
+        super().__init__()
 
     def topk_selection(self, query_embedding: List, num: int):
         """
@@ -22,7 +41,7 @@ class BaseRetriever:
         selection_score = [item[2] for item in selection_results]
         return {"selection_idx": selection_idx, "selection_score": selection_score}
 
-    def get_examples(self, selection_ids: List):
+    def get_examples(self, selection_ids: List) -> List:
         """
 
         :param selection_ids: list of idx
