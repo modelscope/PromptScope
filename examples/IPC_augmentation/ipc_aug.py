2b7ad266ef308cbacef53853bf526a4b428d9290
运泽 <yunze.gy@alibaba-inc.com>
2024-07-16 12:30:31 +0800

    fix __init__ issues

--- examples/IPC_augmentation/ipc_aug.py
+++ examples/IPC_augmentation/ipc_aug.py
@@ -1,4 +1,4 @@
-from meta_icl.core.offline.demonstration_augmentation.ipc import IPC_Generation
+from meta_icl.core.offline.demonstration_augmentation.ipc_aug import IPC_Generation
 import argparse
 import os
 from pathlib import Path
@@ -8,44 +8,14 @@ from meta_icl.core.utils.ipc_config import load_yaml
 from meta_icl.core.enumeration.language_enum import LanguageEnum
 from meta_icl.core.utils.logger import Logger
 
-# General Training Parameters
-# parser = argparse.ArgumentParser()
-
-# parser.add_argument('--prompt',
-#                     default='',
-#                     required=False, type=str, help='Prompt to use as initial.')
-# parser.add_argument('--task_description',
-#                     default='',
-#                     required=False, type=str, help='Describing the task')
-# parser.add_argument('--output_dump', default='dump', required=False, type=str, help='Output to save checkpoints')
-# parser.add_argument('--num_samples', default=10, type=int, help='Total Number of samples to generate')
-# parser.add_argument('--batch_size', default=10, type=int, help='Number of samples in each batch')
-# parser.add_argument('--language', default='cn', type=str, help='Language used by the prompt')
-# parser.add_argument('--workers', default=5, type=int, help='workers for async call')
-
-# opt = parser.parse_args()
-
 logger = Logger.get_logger(__name__)
 basic_config_path = 'ipc_aug_cn.yml'
 
 config_params = load_yaml(basic_config_path)
 logger.info(config_params)
 
-# check language
-# if opt.language.upper() != config_params.language.upper():
-#     raise ValueError("Language inconsistency!")
 if not hasattr(LanguageEnum, config_params.task_config.language.upper()):
     raise NotImplementedError("Only supports 'EN' and 'CN' for now!")
-
-# if opt.task_description == '':
-#     task_description = input("Describe the task: ")
-# else:
-#     task_description = opt.task_description
-
-# if opt.prompt == '':
-#     initial_prompt = input("Initial prompt: ")
-# else:
-#     initial_prompt = opt.prompt
 
 # Initializing the pipeline
 pipeline = IPC_Generation(config_params)
--- meta_icl/core/utils/ipc_base_dataset.py
+++ meta_icl/core/utils/ipc_base_dataset.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from datetime import datetime
 import csv
 
-from meta_icl.core.utils import Dedup
+from meta_icl.core.utils.ipc_dedup import Dedup
 
 class DatasetBase:
     """
