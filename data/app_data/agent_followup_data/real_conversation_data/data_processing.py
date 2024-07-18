ab3b07160ada431ba6c6eb4b3cd247131cca5425
柳佚 <yly287738@alibaba-inc.com>
2024-07-18 20:04:45 +0800

    add agent followups

--- data/app_data/agent_followup_data/real_conversation_data/data_processing.py
+++ data/app_data/agent_followup_data/real_conversation_data/data_processing.py
@@ -10,8 +10,32 @@ if __name__ == '__main__':
     #     excel_file_path='data/app_data/agent_followup_data/real_conversation_data/智能体追问/role_conversation.xlsx',
     #     eval_key_list=[]
     # )
+    # combine_session(
+    #     csv_pth='data/app_data/agent_followup_data/real_conversation_data/智能体追问/role_conversation.csv',
+    #     json_sav_dir='data/app_data/agent_followup_data/real_conversation_data/智能体追问',
+    #     group_by_filed='session_id',
+    #     selection_filed=["name",
+    #                      "instructions",
+    #                      "query",
+    #                      "answer",
+    #                      "log_time"],
+    #     prefix='session_role_conversation',
+    #     # mapping_filed={'question': 'question', 'answer': 'answer'}
+    # )
+    # combine_session(
+    #     csv_pth='data/app_data/agent_followup_data/real_conversation_data/智能体追问/special_case_哄女朋友.csv',
+    #     json_sav_dir='data/app_data/agent_followup_data/real_conversation_data/智能体追问',
+    #     group_by_filed='session_id',
+    #     selection_filed=["name",
+    #                      "instructions",
+    #                      "query",
+    #                      "answer",
+    #                      "log_time"],
+    #     prefix='session_role_conversation_gf',
+    #     # mapping_filed={'question': 'question', 'answer': 'answer'}
+    # )
     combine_session(
-        csv_pth='data/app_data/agent_followup_data/real_conversation_data/智能体追问/role_conversation.csv',
+        csv_pth='data/app_data/agent_followup_data/real_conversation_data/智能体追问/tool_conversation.csv',
         json_sav_dir='data/app_data/agent_followup_data/real_conversation_data/智能体追问',
         group_by_filed='session_id',
         selection_filed=["name",
@@ -19,6 +43,6 @@ if __name__ == '__main__':
                          "query",
                          "answer",
                          "log_time"],
-        prefix='session_role_conversation',
+        prefix='session_tool_conversation',
         # mapping_filed={'question': 'question', 'answer': 'answer'}
     )
