05a1e353d8586b55d2385751c33d24acce1b53cd
运泽 <yunze.gy@alibaba-inc.com>
2024-07-22 18:15:23 +0800

    ipc generation ready

--- examples/IPC_optimization/ipc_optim_generate.py
+++ examples/IPC_optimization/ipc_optim_generate.py
@@ -13,35 +13,41 @@ logger = Logger.get_logger(__name__)
 rank_config_path = 'ipc_ranker.yml'
 generate_config_path = 'ipc_optim_generate.yml'
 
-config_params = load_yaml(rank_config_path)
-logger.info(config_params)
+rank_config_params = load_yaml(rank_config_path)
+generate_config_params = load_yaml(generate_config_path)
+logger.info(rank_config_params)
+logger.info(generate_config_params)
+CONFIG_REGISTRY.batch_register(rank_config_params)
 
-CONFIG_REGISTRY.batch_register(config_params)
-
-if not hasattr(LanguageEnum, config_params.task_config.language.upper()):
+if not hasattr(LanguageEnum, rank_config_params.task_config.language.upper()):
     raise NotImplementedError("Only supports 'EN' and 'CN' for now!")
 
 kwargs, best_prompt = {}, None
-if hasattr(config_params.task_config, 'input_path'):
-    input_path = Path(config_params.task_config.input_path)
+if hasattr(generate_config_params.task_config, 'input_path'):
+    input_path = Path(os.path.join(generate_config_params.task_config.input_path, 'ranking'))
     if (input_path / 'samples.json').is_file():
         with open(input_path / 'samples.json', 'r') as f:
             data = [json.loads(line) for line in f]
         kwargs['data'] = data
     if (input_path / 'history.pkl').is_file():
         state = pkl.load(open(input_path / 'history.pkl', 'rb'))
-        best_prompt = state['cur_prompt']
-        kwargs['ranking_prompt'] = state['cur_prompt']
+        best_prompt = state['prompt']
+        kwargs['ranking_prompt'] = state['prompt']
 
 # Initializing the pipeline
 pipeline = IPC_Optimization()
+
 if not best_prompt:
     kwargs['mode'] = 'ranking'
     best_prompt = pipeline.run_pipeline(**kwargs)
-    kwargs['mode'] = 'generation'
 
-config_params = load_yaml(generate_config_path)
-logger.info(config_params)
-CONFIG_REGISTRY.batch_register(config_params)
+kwargs['mode'] = 'generation'
+pipeline.samples = None
 
+# config_params = load_yaml(generate_config_path)
+# logger.info(config_params)
+CONFIG_REGISTRY.batch_register(generate_config_params)
+# CONFIG_REGISTRY.module_dict['eval_config'].instruction = best_prompt
+pipeline = IPC_Optimization()
+pipeline.init_config()
 best_prompt = pipeline.run_pipeline(**kwargs)
