3022e612900b88a5fad6e689d47f06c1424b4591
运泽 <yunze.gy@alibaba-inc.com>
2024-07-26 18:41:56 +0800

    up

---
+++ meta_icl/core/algorithm/base_algorithm.py
@@ -0,0 +1,63 @@
+from abc import ABC, abstractmethod
+
+class PromptOptimizationWithFeedback(ABC):
+    """
+    Base Abstract Class for Prompt Optimization with Feedback
+    """
+
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def init_model(self):
+        pass
+
+    @abstractmethod
+    def init_config(self):
+        pass
+
+    @abstractmethod
+    def init_prompt(self):
+        pass
+
+    @abstractmethod
+    def run(self):
+        pass
+    
+    @abstractmethod
+    def step(self):
+        pass
+
+    @abstractmethod
+    def extract_best_prompt(self):
+        pass
+    
+    @abstractmethod
+    def stop_criteria(self):
+        pass
+
+class DemonstrationAugmentation(ABC):
+    """
+    Base Abstract Class for Prompt Optimization with Feedback
+    """
+
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def init_model(self):
+        pass
+
+    @abstractmethod
+    def init_config(self):
+        pass
+
+    @abstractmethod
+    def init_prompt(self):
+        pass
+
+    @abstractmethod
+    def run(self):
+        pass
+
+    
