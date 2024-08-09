6b377d6af32084d13b61f02ec3784a8550e58689
运泽 <yunze.gy@alibaba-inc.com>
2024-08-09 18:12:07 +0800

    add openai models

--- tests/test_models_generation.py
+++ tests/test_models_generation.py
@@ -1,8 +1,9 @@
 import unittest
 
-from meta_icl.core.models.generation_model import GenerationModel, AioGenerationModel
+from meta_icl.core.models.generation_model import GenerationModel, AioGenerationModel, OpenAIGenerationModel, OpenAIAioGenerationModel
 from meta_icl.core.utils.logger import Logger
 import asyncio
+import openai
 
 class TestLLILLM(unittest.TestCase):
     """Tests for LlamaIndexGenerationModel"""
@@ -25,23 +26,50 @@ class TestLLILLM(unittest.TestCase):
             "seed": 1234,
         }
         self.async_llm = AioGenerationModel(**async_config)
+
+        openai_config = {
+            "module_name": "openai_generation",
+            "model_name": "gpt-4o-mini",
+            "max_tokens": 200,
+            "top_k": 1,
+            "seed": 1234,
+        }
+        self.openai_llm = OpenAIGenerationModel(**openai_config)
+
+        openai_async_config = {
+            "module_name": "openai_aio_generation",
+            "model_name": "gpt-4o-mini",
+            "max_tokens": 200,
+            "top_k": 1,
+            "seed": 1234,
+        }
+        self.openai_async_llm = OpenAIAioGenerationModel(**openai_config)
         
-        self.logger = Logger.get_logger()
     def test_llm_prompt(self):
         prompt = "你是谁？"
         ans = self.llm.call(stream=False, prompt=prompt, result_format='message')
-        self.logger.info(ans.output.text)
+
+    def test_openai_llm_prompt(self):
+        prompt = "你是谁？"
+        ans = self.openai_llm.call(stream=False, prompt=prompt)
 
     def test_llm_messages(self):
         messages = [{'role': 'system', 'content': 'You are a helpful assistant.'},
                 {'role': 'user', 'content': '你是谁？'}]
         ans = self.llm.call(stream=False, messages=messages, result_format='message')
-        self.logger.info(ans.output.text)
     
+    def test_openai_llm_messages(self):
+        messages = [{'role': 'system', 'content': 'You are a helpful assistant.'},
+                {'role': 'user', 'content': '你是谁？'}]
+        ans = self.openai_llm.call(stream=False, messages=messages)
+
     def test_async_llm_prompt(self):
         prompts = ["你是谁？", "你会什么？", "天气怎么样？"]
         responses = asyncio.run(self.async_llm.async_call(prompts=prompts, result_format='message'))
-        self.logger.info([response.output.text for response in responses])
+
+    def test_openai_async_llm_prompt(self):
+        prompts = ["你是谁？", "你会什么？", "天气怎么样？"]
+        responses = asyncio.run(self.openai_async_llm.async_call(prompts=prompts))
 
     def test_async_llm_messages(self):
         messages = [[{'role': 'system', 'content': 'You are a helpful assistant.'},
@@ -51,21 +79,28 @@ class TestLLILLM(unittest.TestCase):
                 [{'role': 'system', 'content': 'You are a helpful assistant.'},
                 {'role': 'user', 'content': '天气怎么样？'}]]
         responses = asyncio.run(self.async_llm.async_call(list_of_messages=messages, result_format='message'))
-        self.logger.info([response.output.text for response in responses])
+
+    def test_openai_async_llm_messages(self):
+        messages = [[{'role': 'system', 'content': 'You are a helpful assistant.'},
+                {'role': 'user', 'content': '你是谁？'}],
+                [{'role': 'system', 'content': 'You are a helpful assistant.'},
+                {'role': 'user', 'content': '你会什么？'}],
+                [{'role': 'system', 'content': 'You are a helpful assistant.'},
+                {'role': 'user', 'content': '天气怎么样？'}]]
+        responses = asyncio.run(self.async_llm.async_call(list_of_messages=messages))
 
 if __name__ == '__main__':
     # unittest.main()
-
-    config = {
-            "module_name": "generation",
-            "model_name": "qwen-plus",
-            "max_tokens": 2000,
-            "top_k": 1,
+    openai.api_key = ''
+    openai_config = {
+            "module_name": "openai_generation",
+            "model_name": "gpt-4o-mini",
+            "max_tokens": 200,
             "seed": 1234,
         }
-    llm = GenerationModel(**config)
-    prompt = "你是谁？"
+    openai_llm = OpenAIGenerationModel(**openai_config)
+    prompt = "Who are you？"
     messages = [{'role': 'system', 'content': 'You are a helpful assistant.'},
-                {'role': 'user', 'content': '你是谁？'}]
-    ans = llm.call(stream=False, messages=messages, result_format='message')
+                {'role': 'user', 'content': prompt}]
+    ans = openai_llm.call(stream=False, messages=messages)
     print(ans)
